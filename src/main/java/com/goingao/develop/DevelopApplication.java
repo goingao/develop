d2a0ee41ff68e12ab0f452e629a80eabe70687f9
goingao <goingao@163.com>
2019-10-22 11:00:16 +0800

    mq

--- src/main/java/com/goingao/develop/DevelopApplication.java
+++ src/main/java/com/goingao/develop/DevelopApplication.java
@@ -1,13 +1,39 @@
 package com.goingao.develop;
 
+import com.example.demo.service.RocketMqService;
+import com.wisdom.stat.HelloServiceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @SpringBootApplication
+@RestController
 public class DevelopApplication {
+    @Autowired
+    private HelloServiceConfiguration helloService;
+    @Autowired
+    private RocketMqService rocketMqService;
 
     public static void main(String[] args) {
         SpringApplication.run(DevelopApplication.class, args);
     }
 
+    @RequestMapping("/name")
+    public String getName() {
+        return helloService.getName();
+    }
+
+    @RequestMapping("/hobby")
+    public String getHobby() {
+        return helloService.getHobby();
+    }
+
+    @RequestMapping("/rockMq")
+    public String getTopic() {
+        return rocketMqService.getTopic();
+    }
 }
